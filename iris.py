3cd88da87e32fb13440236fbed4d1dbe4d0b38c1
yt342 <143104227+yt342@users.noreply.github.com>
2026-03-14 16:08:58 +0530

    Create iris.py

---
+++ iris.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.title("Iris Prediction")
+import pickle
+model=pickle.load(open("model_iris.pkl","rb"))
+sl=st.slider("Sl",2.0,10.0)
+sw=st.slider("Sw",2.0,10.0)
+pl=st.slider("pl",2.0,10.0)
+pw=st.slider("pw",2.0,10.0)
+if st.button("Predict"):
+    st.success(model.predict([[sl,sw,pl,pw]]))
