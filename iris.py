291bedeaccc1a78892cca96b122a02ef9cebeabd
yt342 <143104227+yt342@users.noreply.github.com>
2026-03-15 09:06:06 +0530

    Update iris.py

--- iris.py
+++ iris.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("Iris Prediction")
 import pickle
-model=pickle.load(open("model_iris.pkl","rb"))
+model=pickle.load(open("model_svm.pkl","rb"))
 sl=st.slider("Sl",2.0,10.0)
 sw=st.slider("Sw",2.0,10.0)
 pl=st.slider("pl",2.0,10.0)
